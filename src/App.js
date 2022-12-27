1b120e134d2466fdd437692cc2cc36bbc0829057
Che Van Thao <chevanthao96@gmail.com>
2022-12-27 10:38:48 +0900

    set up fake rest api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Greeting } from './components/Greeting';
+import { PostList } from './components/Post';
 
 function App() {
   return (
     <div className="App">
       <Greeting></Greeting>
+      <PostList></PostList>
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+export const PostList = () => {
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => {
+            setPost(data)
+        })
+    },[])
+
+    return (
+        <div>
+            {post.map((item, index) => (
+                <li key={index}>{item.title}</li>
+                ))}
+        </div>
+    )
+}
