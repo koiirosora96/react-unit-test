8c33d89d7a043786b96849cd735115c7570c751c
Che Van Thao <chevanthao96@gmail.com>
2022-12-27 11:36:56 +0900

    add api mock

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -3,6 +3,10 @@ import { PostList } from './Post'
 
 describe('test fake rest api',() => {
     test('test render list', async () => {
+        window.fetch = fetch.fn()
+        window.fetch.mockResolvedValueOne({
+            json: async () => [{id: '1', title: 'First Post'}]
+        })
         render(<PostList/>)
 
         const listItemElement = await screen.findAllByRole('listitem', {timeout: 10})
